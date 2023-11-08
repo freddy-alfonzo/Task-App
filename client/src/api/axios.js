9693374688e9f7e2d14f4a7d41b4bc6da0db1501
Freddy Alfonzo Lozada <freddy.alfonzo01@Gmail.com>
2023-11-08 17:23:54 -0400

    cors rlly hate u

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"https://freddy-task-app.vercel.app/api",
+    baseURL:"https://server-freddy.vercel.app/api",
     withCredentials: true
 })
 export default instance
