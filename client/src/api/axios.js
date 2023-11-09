babddf33899eb63cf447bc50b500d45a8e9a1b72
Freddy Alfonzo Lozada <freddy.alfonzo01@Gmail.com>
2023-11-09 13:11:19 -0400

    remove withCredentials

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"https://server-freddy.vercel.app/api",
-    withCredentials: true
+    baseURL:"https://server-freddy.vercel.app/api"
 })
 export default instance
