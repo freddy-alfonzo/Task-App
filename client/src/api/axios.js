126fe1ed568b3ec4eec0912cfaf24af9abfeb257
Freddy Alfonzo Lozada <freddy.alfonzo01@Gmail.com>
2023-11-09 13:40:50 -0400

    elloo

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"https://server-freddy.vercel.app/api"
+    baseURL:"https://server-freddy.vercel.app/api",
+    withCredentials:"true"
 })
 export default instance
